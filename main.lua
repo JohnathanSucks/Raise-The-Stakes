a449a5b5102345b8f1528dd4b00bdd759a888fdd
JohnathanSucks <121587099+JohnathanSucks@users.noreply.github.com>
2025-01-10 15:23:55 -0600

    Add files via upload

---
+++ main.lua
@@ -0,0 +1,46 @@
+--- STEAMODDED HEADER
+--- MOD_NAME: Raise The Stakes
+--- MOD_ID: RAISETHESTAKES
+--- MOD_AUTHOR: [JohnathanSucks]
+--- MOD_DESCRIPTION: A simple music replacement mod that switches the normal Balatro themes to the Raise The Stakes themes
+--- PREFIX: rts
+-----------------------------------------------------------
+------------------MOD CODE---------------------------------
+
+SMODS.Sound({
+    pitch = 1,
+    key = "music_main_theme",
+    path = "music1.ogg",
+    replace = 'music1',
+})
+
+SMODS.Sound({
+    pitch = 1,
+    key = "music_shop_theme",
+    path = "music4.ogg",
+    replace = 'music4',
+})
+
+SMODS.Sound({
+    pitch = 1,
+    key = "music_arcana_theme",
+    path = "music2.ogg",
+    replace = 'music2',
+})
+
+SMODS.Sound({
+    pitch = 1,
+    key = "music_planet_theme",
+    path = "music3.ogg",
+    replace = 'music3',
+})
+
+SMODS.Sound({
+    pitch = 1,
+    key = "music_boss_theme",
+    path = "music5.ogg",
+    replace = 'music5',
+})
+
+-----------------------------------------------------------
+------------------MOD CODE END---------------------------------
